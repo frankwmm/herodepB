6f052005a0e97df0923b0e71efbf4141a0431641
frankwmm <frankwilsonknow@gmail.com>
2021-06-27 18:47:06 -0500

    WGC up M/I rtrt

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 const express = require("express");
 const path = require("path");
 const app = express();
-app.use(express.static(__dirname + "/herodep-b/dist/"));
-app.get("*", function (req, res) {
-  res.sendFile("./herodep-b/dist/index.html");
+app.use(express.static(__dirname + "/dist/herodep-b"));
+app.get("/*", function (req, res) {
+  res.sendFile(path.join(__dirname + "/dist/herodep-b/index.html"));
 });
 app.listen(process.env.PORT || 8080);
